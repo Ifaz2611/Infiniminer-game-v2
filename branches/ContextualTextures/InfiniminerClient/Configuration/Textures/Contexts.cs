b720a0e380a714b69cd57d89110fb0d1dec932a3
Ifaz Md Zahin <zahin26112004@gmail.com>
2025-04-30 21:58:38 +0600

    update-gamev2

---
+++ branches/ContextualTextures/InfiniminerClient/Configuration/Textures/Contexts.cs
@@ -0,0 +1,91 @@
+﻿namespace Infiniminer
+{
+    public class Contexts
+    {
+        public const byte exposedToSunlight = 1;
+        public const byte exposedToLava     = 2;
+
+        public const byte obscuredBySolid   = 4; // if a block isn't exposed by these two,
+        public const byte obscuredByTrans   = 8; // then it is exposed to air
+
+        public const byte tunnelNorth  = 16;
+        public const byte tunnelEast   = 32;
+        public const byte tunnelWest   = 64;
+        public const byte tunnelSouth  = 128;
+
+        public static BlockTexture Texture(ushort x, ushort y, ushort z, BlockFaceDirection faceDir, BlockType type, BlockType[, ,] downloadList, BlockTexture[,] blockTextureMap)
+        {
+            BlockTexture blockTexture = blockTextureMap[(byte)type, (byte)faceDir];
+            switch (type)
+            {
+                case BlockType.Dirt:
+                    {
+                        if (
+                            configHelper.isAboveGround(x,y,z)
+                        ){
+                            switch (faceDir)
+                            {
+                                case BlockFaceDirection.YIncreasing:
+                                {
+                                    if (configHelper.isOutOfBounds(x, y + 2, z) || downloadList[x, y + 1, z] == BlockType.None)
+                                    {
+                                        blockTexture = BlockTexture.Grass;
+                                    }
+                                }
+                                break;
+                                case BlockFaceDirection.XDecreasing:
+                                {
+                                    if (configHelper.isOutOfBounds(x - 1, y, z) || downloadList[x - 1, y, z] == BlockType.None)
+                                    {
+                                        blockTexture = BlockTexture.Grass;
+                                    }
+                                }
+                                break;
+                                case BlockFaceDirection.XIncreasing:
+                                {
+                                    if (configHelper.isOutOfBounds(x + 1, y, z) || downloadList[x + 1, y, z] == BlockType.None)
+                                    {
+                                        blockTexture = BlockTexture.Grass;
+                                    }
+                                }
+                                break;
+                                case BlockFaceDirection.ZDecreasing:
+                                {
+                                    if (configHelper.isOutOfBounds(x, y, z - 1) || downloadList[x, y, z - 1] == BlockType.None)
+                                    {
+                                        blockTexture = BlockTexture.Grass;
+                                    }
+                                }
+                                break;
+                                case BlockFaceDirection.ZIncreasing:
+                                {
+                                    if (configHelper.isOutOfBounds(x, y, z + 1) || downloadList[x, y, z + 1] == BlockType.None)
+                                    {
+                                        blockTexture = BlockTexture.Grass;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        else if (configHelper.isGround(x, y, z))
+                        {
+                            switch (faceDir)
+                            {
+                                case BlockFaceDirection.YIncreasing:
+                                    blockTexture = BlockTexture.Grass;
+                                    break;
+                                case BlockFaceDirection.XDecreasing:
+                                case BlockFaceDirection.XIncreasing:
+                                case BlockFaceDirection.ZDecreasing:
+                                case BlockFaceDirection.ZIncreasing:
+                                    blockTexture = BlockTexture.DirtGrass;
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+            }
+            return blockTexture;
+        }
+    }
+}
---
+++ branches/Marvulous Mod/code/InfiniminerClient/Configuration/GlobalVariables.cs
@@ -0,0 +1,42 @@
+namespace Infiniminer
+{
+    public class GlobalVariables
+    {
+        public const byte MAPSIZE = 64;
+        public const string INFINIMINER_VERSION = "v1.5";
+        public const int GROUND_LEVEL = 8;
+
+        public const ushort goldCash = 100;
+        public const ushort diamondCash = 1000;
+        public const byte goldWeight = 1;
+        public const byte diamondWeight = 1;
+
+        public const bool sandboxMode = false;
+
+        // block type factors
+        public const uint oreFactor = 20;
+
+        // Display constants
+        public const ushort minScreenWidth = 320;
+        public const ushort maxScreenWidth = 1440;
+        public const ushort minScreenHeight = 240;
+        public const ushort maxScreenHeight = 1080;
+
+        // Shader effects
+        public const string fx__effect_skyplane    = "effects/effect_skyplane";
+        public const string fx__effect_basic       = "effects/effect_basic";
+        public const string fx__effect_particle    = "effects/effect_particle";
+        public const string fx__effect_spritemodel = "effects/effect_spritemodel";
+        public const string fx__BloomExtract       = "effects/BloomExtract";
+        public const string fx__BloomCombine       = "effects/BloomCombine";
+        public const string fx__GaussianBlur       = "effects/GaussianBlur";
+
+        // fonts
+        public const string fontUI    = "fonts/04b08";
+        public const string fontRadar = "fonts/04b03b";
+        public const string fontName  = fontUI;
+
+        // Render mods
+        public const bool naturalEnvironments = false;
+    }
+}
